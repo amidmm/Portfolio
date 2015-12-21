152afd7530ef0107c15b31d7bb3fa3e8efb3ac55
Seyed Amid Moeinzadeh Mirhosseini <Amid.moeinzadeh@gmail.com>
2015-12-21 18:45:16 +0330

    screen one 3 bar
    Add 3 vertical bar in the screen one

--- Portfolio/Portfolio/js/Script.js
+++ Portfolio/Portfolio/js/Script.js
@@ -1,8 +1,9 @@
 ﻿jQuery(document).ready(function ($) {
+    $('#pic1-6').css('left', $('#pic1-5').width()+'px');
     var test = 0;
     $(window).scroll(function () {
         var scrolled = $(this).scrollTop();
-        var myPicPos = $('#myPic').position();
+        var myPicPos = $('#myPic').offset();
         var h = $(window).height();
         if (scrolled <= $(window).width() + 20) {
             $('.pic1').css('display', 'block');
@@ -11,16 +12,19 @@
             $('#pic1-2').css('transform', 'translateX(-' + scrolled + 'px)');
             $('#pic1-3').css('transform', 'translateX(' + scrolled + 'px)');
             $('#pic1-4').css('transform', 'translateX(-' + scrolled + 'px)');
-            /*if (myPicPos.left < $(window).width()/2-20) {
+            $('#pic1-5').css('transform', 'translateY(' + scrolled + 'px)');
+            $('#pic1-6').css('transform', 'translateY(-' + scrolled + 'px)');
+            $('#pic1-7').css('transform', 'translateY(' + scrolled + 'px)');
+            if (myPicPos.left < $(window).width() / 2 + $('#myPic').width()) {
                 $('#myPic').css('transform', 'translateX(' + scrolled + 'px)');
                 test = scrolled;
             }
             if (myPicPos.left > $(window).width() / 2) {
-                $('#myPic').css('transform', 'translateX(' + $(window).width() / 2 - 200 + 'px)');
+                $('#myPic').css('transform', 'translateX(' + ($(window).width() / 2) - $('#myPic').width() + 'px)');
             }
             if (scrolled <= test && myPicPos.left >= $(window).width() / 2) {
                 $('#myPic').css('transform', 'translateX(-' + scrolled + 'px)');
-            }*/
+            }
             
         }
         else if (scrolled < $(window).width()*2) {
