21f05e791fca6c4c03e51e70c7dcc2bd9f10f88c
Seyed Amid Moeinzadeh Mirhosseini <Amid.moeinzadeh@gmail.com>
2015-12-21 15:37:27 +0330

    screen one
    screen one is complete but the picture should get replaced with the real
    one

---
+++ Portfolio/Portfolio/js/Script.js
@@ -0,0 +1,11 @@
+﻿jQuery(document).ready(function ($) {
+    $(window).scroll(function () {
+        var scrolled = $(this).scrollTop();
+        if (scrolled < $(document).height()) {
+            $('#pic1-1').css('transform', 'translateX(' + scrolled + 'px)');
+            $('#pic1-2').css('transform', 'translateX(-' + scrolled + 'px)');
+            $('#pic1-3').css('transform', 'translateX(' + scrolled + 'px)');
+            $('#pic1-4').css('transform', 'translateX(-' + scrolled + 'px)');
+        };
+    });
+});
