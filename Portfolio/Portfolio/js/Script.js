ff1f8bf0b72ba036ad2a1fd6e09e40bec63425d0
Seyed Amid Moeinzadeh Mirhosseini <Amid.moeinzadeh@gmail.com>
2015-12-22 14:08:31 +0330

    Screen three
    changed pic2 to pic1 (in pervious code)
    Add screen three
    fix bug for #myPic

--- Portfolio/Portfolio/js/Script.js
+++ Portfolio/Portfolio/js/Script.js
@@ -5,7 +5,7 @@
         var scrolled = $(this).scrollTop();
         var myPicPos = $('#myPic').offset();
         var h = $(window).height();
-        if (scrolled <= $(window).width() + 20) {
+        if (scrolled <= $(window).width()/2) {
             $('.pic1').css('display', 'block');
             $('.pic2').css('display', 'none');
             $('#pic1-1').css('transform', 'translateX(' + scrolled + 'px)');
@@ -15,23 +15,27 @@
             $('#pic1-5').css('transform', 'translateY(' + scrolled + 'px)');
             $('#pic1-6').css('transform', 'translateY(-' + scrolled + 'px)');
             $('#pic1-7').css('transform', 'translateY(' + scrolled + 'px)');
-            if (myPicPos.left < $(window).width() / 2 + $('#myPic').width()) {
-                $('#myPic').css('transform', 'translateX(' + scrolled + 'px)');
-                test = scrolled;
-            }
-            if (myPicPos.left > $(window).width() / 2) {
-                $('#myPic').css('transform', 'translateX(' + ($(window).width() / 2) - $('#myPic').width() + 'px)');
-            }
-            if (scrolled <= test && myPicPos.left >= $(window).width() / 2) {
-                $('#myPic').css('transform', 'translateX(-' + scrolled + 'px)');
-            }
+            $('#myPic').css('transform', 'translateX(' + Math.min(scrolled, $(window).width() / 2)+'px)');
             
         }
-        else if (scrolled < $(window).width()*2) {
+        else if (scrolled <= $(window).width()) {
+
+        }
+        else if (scrolled <= $(window).width() * 2) {
+            $('.pic1').css('display', 'block');
+            $('.pic2').css('display', 'none');
+            $('#pic1-1').css('transform', 'translateX(' + scrolled/2 + 'px)');
+            $('#pic1-2').css('transform', 'translateX(-' + scrolled/2 + 'px)');
+            $('#pic1-3').css('transform', 'translateX(' + scrolled/2 + 'px)');
+            $('#pic1-4').css('transform', 'translateX(-' + scrolled/2 + 'px)');
+            $('#pic1-5').css('transform', 'translateY(' + scrolled/2 + 'px)');
+            $('#pic1-6').css('transform', 'translateY(-' + scrolled/2 + 'px)');
+            $('#pic1-7').css('transform', 'translateY(' + scrolled/2 + 'px)');
+            
+        }
+        else if (scrolled < $(window).width()*4) {
             $('.pic1').css('display', 'none');
             $('.pic2').css('display', 'block');
-            $('.pic2').fadeIn('fast');
-            
         };
     });
 });
