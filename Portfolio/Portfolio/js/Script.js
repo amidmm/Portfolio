6230eaac89fec37f75769f0fa8e255ca03c27499
Seyed Amid Moeinzadeh Mirhosseini <Amid.moeinzadeh@gmail.com>
2015-12-24 14:29:11 +0330

    ratio fix
    ratio fix to work on any device

--- Portfolio/Portfolio/js/Script.js
+++ Portfolio/Portfolio/js/Script.js
@@ -1,12 +1,17 @@
 ﻿jQuery(document).ready(function ($) {
-    $('#rest').css('height', $(window).width() * 4 -$('.picContainer').height()/3 + 'px');
+    var ratio = 2.02248875 * $(window).height() / $(window).width();
+    $('#rest').css('height', $(window).width() * 4 *ratio - $('.picContainer').height()*ratio / 3 + 'px');
     $('#pic1-6').css('left', $('#pic1-5').width() + 'px');
+    $('#pic1-6').css('height', $(window).height() * 1.1 + 'px');
+    $('#pic1-5').css('height', $(window).height() * 1.1 + 'px');
+    $('#pic1-7').css('height', $(window).height() * 1.1 + 'px');
     var test = 0;
     $(window).scroll(function () {
         var scrolled = $(this).scrollTop();
         var myPicPos = $('#myPic').offset();
         var h = $(window).height();
-        if (scrolled <= $(window).width() / 2) {
+        if (scrolled <= $(window).width()*ratio / 2) {
+            $.scrollify.enable();
             $('.pic1').css('display', 'block');
             $('.pic2').css('display', 'none');
             $('.pic3').css('display', 'none');
@@ -18,12 +23,11 @@
             $('#pic1-6').css('transform', 'translateY(-' + scrolled + 'px)');
             $('#pic1-7').css('transform', 'translateY(' + scrolled + 'px)');
             $('#myPic').css('transform', 'translateX(' + Math.min(scrolled, $(window).width() / 2) + 'px)');
-
         }
-        else if (scrolled <= $(window).width()) {
-
+        else if (scrolled <= $(window).width()*ratio) {
+            $.scrollify.enable();
         }
-        else if (scrolled <= $(window).width() * 2) {
+        else if (scrolled <= $(window).width() * 2*ratio) {
             $('.pic1').css('display', 'block');
             $('.pic2').css('display', 'none');
             $('.pic3').css('display', 'none');
@@ -37,14 +41,14 @@
             $('#myPic').fadeIn('slow');
 
         }
-        else if (scrolled < $(window).width() * 3) {
+        else if (scrolled < $(window).width() * 3 *ratio) {
             $('.pic1').css('display', 'none');
             $('.pic2').css('display', 'block');
             $('.pic3').css('display', 'none');
             $('#myPic').fadeOut('slow');
         }
         else {
-            var tempY = scrolled - $(window).width() * 3;
+            var tempY = scrolled - $(window).width() * 3 * ratio;
             $('#fPic').css('transform', 'translateY(-' + tempY + 'px)');
             $('#sPic').css('transform', 'translateY(-' + tempY + 'px)');
             $('#tPic').css('transform', 'translateY(-' + tempY + 'px)');
@@ -52,5 +56,8 @@
             $('.pic2').css('display', 'none');
             $('.pic3').css('display', 'block');
         };
+    });
+    $('#popup').click(function () {
+        $(this).css('display', 'none');
     });
 });
